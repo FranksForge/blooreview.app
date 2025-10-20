9699ab77239f25df1c922e8ab5d093f6d865ab4b
Frank <f.rust95@web.de>
2025-10-20 10:21:29 +0700

    Update configs

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// Multi-tenant configs - updated: 2025-10-20T03:16:02.772Z
+// Multi-tenant configs - updated: 2025-10-20T03:19:45.302Z
 // DO NOT EDIT MANUALLY - Run 'node sync-config.js' to update from Google Sheets
 window.REVIEW_CONFIGS = {
   "bigc-donchan": {
